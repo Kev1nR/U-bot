6e17ea4074a471ddbc060c72e080c558f903a5d9
pi <pi@RPiZero.local>
2022-12-31 16:44:50 +0000

    update

--- Part2-HostAWebPage/helloWorld.py
+++ Part2-HostAWebPage/helloWorld.py
@@ -1,6 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import datetime
-import ViaHBridge
+import ViaHBridge2 as motion
 
 app = Flask(__name__)
 @app.route("/")
@@ -8,38 +8,122 @@ def hello():
    now = datetime.datetime.now()
    timeString = now.strftime("%Y-%m-%d %H:%M")
    templateData = {
-      'title' : 'U-bot dashboard'
-      }
+      'title' : 'U-bot dashboard',
+      'slowspeed' : 'speedButton-off',
+      'mediumspeed' : 'speedButton-off',
+      'fastspeed' : 'speedButton-off',
+      'fullspeed' : 'speedButton-off',
+      'fbutton' : 'button',
+      'revbutton' : 'button',
+      'rbutton' : 'button',
+      'lbutton' : 'button'}
    return render_template('index.html', **templateData)
 
-@app.route("/<direction>")
-def move(direction):
-   
+@app.route("/motion")
+def move():
+   ts = motion.TravelStatus(motion.TravelMode.Stop, 0.0)
+
+   direction = request.args.get('direction')
+   speed = request.args.get('speed')
+
+   if (speed == ''):
+      speed = 'zero'
+   if (direction == ''):
+      direction = 'stop'
+
+   print ("direction is ", direction, ". Speed is ", speed) 
    if (direction == 'forward'):
      print ("Move forward")
-     ViaHBridge.forwardDrive(0.2)
-
+     ts.setTravelMode(motion.TravelMode.Forward)
+     #.and motion .forwardDrive(0.2)
+     
    if (direction == 'reverse'):
      print ("Move backward")
-     ViaHBridge.reverseDrive(0.2)
+     ts.setTravelMode(motion.TravelMode.Reverse)
+
+#     motion.reverseDrive(0.2)
 
    if (direction == 'left'):
      print ("Turn left")
-     ViaHBridge.spinAntiClockwise(0.2)
+     ts.setTravelMode(motion.TravelMode.TurnLeft)
+     #motion.spinAntiClockwise(0.2)
 
    if (direction == 'right'):
      print ("Turn right")
-     ViaHBridge.spinClockwise(0.2)
+     ts.setTravelMode(motion.TravelMode.TurnRight)
+      #motion.spinClockwise(0.2)
 
    if (direction == 'stop'):
      print ("Stop")
-     ViaHBridge.allStop()
-    
+     ts.setTravelMode(motion.TravelMode.Stop)
+   #motion.allStop()  
 
+   if (speed == 'zero'):
+    slowSpeed = 'speedButton-off'
+    mediumSpeed = 'speedButton-off'
+    fastSpeed = 'speedButton-off'
+    fullSpeed = 'speedButton-off'
+    ts.setSpeed(0.0)
+
+   if (speed == 'slow'):
+    slowSpeed = 'speedButton-1'
+    mediumSpeed = 'speedButton-off'
+    fastSpeed = 'speedButton-off'
+    fullSpeed = 'speedButton-off'
+    ts.setSpeed(0.2)
+    
+   if (speed == 'medium'):
+    slowSpeed = 'speedButton-1'
+    mediumSpeed = 'speedButton-2'
+    fastSpeed = 'speedButton-off'
+    fullSpeed = 'speedButton-off'
+    ts.setSpeed(0.5)
+    
+   if (speed == 'fast'):
+    slowSpeed = 'speedButton-1'
+    mediumSpeed = 'speedButton-2'
+    fastSpeed = 'speedButton-3'
+    fullSpeed = 'speedButton-off'
+    ts.setSpeed(0.75)
+
+   if (speed == 'full'):
+    slowSpeed = 'speedButton-1'
+    mediumSpeed = 'speedButton-2'
+    fastSpeed = 'speedButton-3'
+    fullSpeed = 'speedButton-4'
+    ts.setSpeed(1.0)
+    
+   fbutton = 'button'
+   revbutton = 'button'
+   rbutton = 'button'
+   lbutton = 'button'
+   
+   if (direction == 'forward'):
+    fbutton = 'button-off'
+   if (direction == 'reverse'):
+    revbutton = 'button-off'
+   if (direction == 'right'):
+    rbutton = 'button-off'
+   if (direction == 'left'):
+    lbutton = 'button-off'
+   
+   if (speed == ''):
+      speed = 'zero'
+      
    templateData = {
-      'direction' : direction 
+      'direction' : direction, 
+      'speed' : speed,
+      'slowspeed' : slowSpeed,
+      'mediumspeed' : mediumSpeed,
+      'fastspeed' : fastSpeed,
+      'fullspeed' : fullSpeed,
+      'fbutton' : fbutton,
+      'revbutton' : revbutton,
+      'rbutton' : rbutton,
+      'lbutton' : lbutton   
       }
    return render_template('index.html', **templateData)
+
 
 if __name__ == "__main__":
    app.run(host='0.0.0.0', port=80, debug=True)
