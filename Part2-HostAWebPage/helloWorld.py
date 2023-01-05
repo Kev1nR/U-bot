db5015bc90ae4ef70f2873b026d996ff4f366c72
pi <pi@RPiZero.local>
2023-01-05 19:22:29 +0000

    Stub camera module

--- Part2-HostAWebPage/helloWorld.py
+++ Part2-HostAWebPage/helloWorld.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, Response
 import datetime
 import ViaHBridge2 as motion
+from Camera import Camera
 
 app = Flask(__name__)
 @app.route("/")
@@ -124,6 +125,17 @@ def move():
       }
    return render_template('index.html', **templateData)
 
+def gen(camera):
+    while True:
+        frame = camera.get_frame()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+@app.route('/video_feed')
+def video_feed():
+#   return ""
+    return Response(gen(Camera()),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == "__main__":
    app.run(host='0.0.0.0', port=80, debug=True)
